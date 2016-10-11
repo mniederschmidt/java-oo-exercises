2f02cd98f260e6d4745866f9709f8fba69fa0bdf
mniederschmidt <mary.niederschmidt@gmail.com>
2016-10-11 00:31:39 -0500

    Gradebook Assignment

--- java-track/assignments/Course.java
+++ java-track/assignments/Course.java
@@ -1,4 +1,83 @@
 
 public class Course {
-// test
+
+	private String name;
+	private int credits;
+	private int seats;
+	private int seatsRemaining;
+	private Student[] studentRoster;
+	
+	public Course(String name, int credits, int seats) {
+		this.name = name;
+		this.credits = credits;
+		this.studentRoster = new Student[seats];
+		this.seats = seats;
+		this.seatsRemaining = seats;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getCredits() {
+		return this.credits;
+	}
+	
+	public int getRemainingSeats() {
+		return this.seatsRemaining;
+	}
+	
+	public int getFilledSeats() {
+		return this.seats - this.seatsRemaining;
+	}
+	
+	
+	public boolean addStudent(Student newStudent) {
+		int filledSeats = this.getFilledSeats();
+		
+		// check that seats are available
+		if (this.seatsRemaining == 0) {
+			return false;
+		}
+		
+		// check that student is not already enrolled
+		for (int i = 0; i < filledSeats; ++i) {
+			if (this.studentRoster[i].getName().equals(newStudent.getName())) {
+				return false;
+			}
+		}
+		
+		// enroll student
+		this.studentRoster[filledSeats] = newStudent;
+		this.seatsRemaining--;
+		return true;
+	}
+	
+	public double averageGPA() {
+		double sumGPA = 0;
+		int filledSeats = this.getFilledSeats();
+		
+		for (int i = 0; i < filledSeats; ++i) {
+			sumGPA += this.studentRoster[i].getGPA();
+		}
+		
+		return sumGPA / filledSeats;
+	}
+	
+	public String generateRoster() {
+		String roster = "Class Roster: ";
+		int filledSeats = this.getFilledSeats();
+		
+		for (int i = 0; i < filledSeats; ++i) {
+			roster = roster + "\n" + this.studentRoster[i].getName();
+		}	
+		return roster;
+	}
+	
+	public String toString() {
+		return "Name: " + this.name + 
+			   "\nCredits:" + this.credits +
+			   "\nSeats Remaining: " + this.seatsRemaining +
+			   "\n" + this.generateRoster();
+	}
 }
--- java-track/assignments/Student.java
+++ java-track/assignments/Student.java
@@ -1,4 +1,128 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Student {
+	
+	private String firstName;
+	private String lastName;
+	private int studentID;
+	private int credits;
+	private double GPA;
+	private double qualityScoreTotal;
+	
+	public Student(String firstName, String lastName, int studentID) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.studentID = studentID;
+		this.credits = 0;
+		this.GPA = 0;
+		this.qualityScoreTotal = 0;
+	}
+	
+	public Student(Student mom, Student dad) {
+		this.firstName = mom.getName();
+		this.lastName = dad.getName();
+		this.studentID = mom.getStudentID() + dad.getStudentID();
+		this.GPA = (mom.getGPA() + dad.getGPA()) / 2;
+		if (mom.getCredits() > dad.getCredits()) {
+			this.credits = mom.getCredits();
+		} 
+		else {
+			this.credits = dad.getCredits();
+		}
+		this.qualityScoreTotal = this.credits * this.GPA;
+	}
+	
+	public String getName() {
+		return this.firstName + " " + this.lastName;
+	}
+	
+	public int getStudentID() {
+		return this.studentID;
+	}
+	
+	public int getCredits() {
+		return this.credits;
+	}
+	
+	public double getGPA() {
+		return this.GPA;
+	}
+	
+	private double calculateGPA() {
+		if (this.credits > 0) {
+			return round((this.qualityScoreTotal / this.credits), 3);
+		} 
+		else {
+			return 0;
+		}
+	}
+	
+	public String getClassStanding() {
+		if (this.credits < 30) {
+			return "Freshman";
+		} else if (this.credits < 60) {
+			return "Sophomore";
+		} else if (this.credits < 90) {
+			return "Junior";
+		} else {
+			return "Senior";
+		}
+	}
+	
+	// Update: credits, qualityScoreTotal, and GPA
+	public void submitGrade(double grade, int credits) {
+		this.qualityScoreTotal += credits * grade;
+		this.credits += credits;
+		this.GPA = calculateGPA();
+	}
+	
+	public double computeTuition() {
+		double currentSemesterCost = 20000;
+		int semestersStudied = this.credits / 15;
+		int partialCredits = this.credits % 15;
+		
+		double totalCost = semestersStudied * currentSemesterCost;
+		if (partialCredits > 0) {
+			totalCost += partialCredits * 1333.33;
+		}
 
+		return totalCost;
+		
+//		BigDecimal costPerCreditHour = new BigDecimal(currentSemesterCost / 15);
+//		BigDecimal credits = new BigDecimal(this.credits);
+//		BigDecimal tuitionCalculated = costPerCreditHour.multiply(credits);
+//	    tuitionCalculated.setScale(2, RoundingMode.FLOOR);
+//		double tuition = tuitionCalculated.doubleValue();
+//		System.out.println("tuition calculated: " + tuitionCalculated);
+//		return tuition;
+	}
+	
+	public Student createLegacy(Student mom, Student dad) {
+		return new Student(mom, dad);		
+	}
+	
+	public String toString() {
+		return "First Name: " + this.firstName + 
+			   "\nLast Name:" + this.lastName +
+			   "\nStudent ID: " + this.studentID +
+			   "\nCredits: " + this.credits +
+			   "\nGPA: " + this.GPA +
+			   "\nPoints Earned: " + this.qualityScoreTotal;
+	}
+	
+	public static void main(String[] args) {
+		Student myStudent = new Student("Mary", "Niederschmidt", 123456);
+		myStudent.submitGrade(2.0, 3);
+		System.out.println(myStudent);
+	}
+	
+	private static double round(double value, int places) {
+	    if (places < 0) throw new IllegalArgumentException();
+
+	    BigDecimal bd = new BigDecimal(value);
+	    bd = bd.setScale(places, RoundingMode.HALF_UP);
+	    return bd.doubleValue();
+	}
+	
 }
